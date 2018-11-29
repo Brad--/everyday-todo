33318533cdfd638df83c6e2ebf74ba4a9cde0183
Brad Smith <bradleys@hpe.com>
2018-11-29 08:57:08 -0700

    Add list-append & storage service. Forgot to add!

---
+++ src/app/list-append/list-append.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ListComponent } from '../list/list.component';
+import { ListItem } from '../list/model/list-item.model';
+
+import { faBan } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition
+} from '@angular/animations';
+
+@Component({
+    selector: 'list-append',
+    templateUrl: './list-append.component.html',
+    styleUrls: ['../list/list.component.css','./list-append.component.css'],
+    animations: [trigger('fadeInOut', [
+        state('out', style({
+            opacity: .2
+        })),
+        state('in', style({
+            opacity: 1
+        })),
+        transition('out => in', [
+            animate('.25s')
+        ]),
+        transition('in => out', [
+            animate('.20s')
+        ])
+    ])]
+})
+export class ListAppendComponent extends ListComponent {
+    faBan = faBan;
+    faPlus = faPlus;
+
+    appendValueAndClear(append: any) {
+        this.addListItemForValue(append.value);
+        append.value = '';
+    }
+
+    onRemoveClicked(id: number) {
+        // Fade the list item out then delete
+        this.removeListItem(id);
+    }
+}
---
+++ src/app/services/list-storage.service.ts
@@ -0,0 +1,30 @@
+import { Inject, Injectable } from '@angular/core';
+import { SESSION_STORAGE, StorageService } from 'angular-webstorage-service';
+
+import { ListItem } from '../list/model/list-item.model';
+
+const LIST_STORAGE_KEY = 'local_list';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ListStorageService {
+    constructor (@Inject(SESSION_STORAGE) private storage: StorageService) {
+
+    }
+
+    public getCurrentListFromLocalStorage(): ListItem[] {
+        let currentList = this.storage.get(LIST_STORAGE_KEY) || [];
+        for (let i = 0; i < currentList.length; i++) {
+            let item = currentList[i];
+            // All items load faded in
+            let listItem = new ListItem(item.value, item.checked, item.id, item.lastUpdate, 'in');
+            currentList[i] = listItem;
+        }
+        return currentList;
+    }
+
+    public updateLocalStorage(list: ListItem[]) {
+        this.storage.set(LIST_STORAGE_KEY, list);
+    }
+}
